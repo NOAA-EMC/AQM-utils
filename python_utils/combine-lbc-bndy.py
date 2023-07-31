a92c31b22312c2e2b57102ae1d88870d1e47e71e
zmoon <zachary.moon@noaa.gov>
2023-07-31 16:38:45 +0000

    Parse passed fhs

---
+++ python_utils/combine-lbc-bndy.py
@@ -0,0 +1,27 @@
+"""
+Combine LBC files from GFS into one
+"""
+import argparse
+
+import netCDF4 as nc
+
+parser = argparse.ArgumentParser(description="Combine LBCs.")
+
+parser.add_argument(
+    "--hours",
+    nargs="+",
+    help=(
+        "Forecast hour strings (array, don't surround it with outer quotes). "
+        "Not including forecast hour 0. "
+        "Example: `003 006`."
+    ),
+    required=True,
+)
+
+args = parser.parse_args()
+print(args)
+
+fh_strings = args.hours
+fh_strings.insert(0, "0" * len(fh_strings[0]))
+print(fh_strings)
+
