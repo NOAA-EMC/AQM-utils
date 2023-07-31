81965570b4f4b6c6bb0ace9b94863b4853045100
zmoon <zachary.moon@noaa.gov>
2023-07-31 22:10:57 +0000

    Append

--- python_utils/combine-lbc-bndy.py
+++ python_utils/combine-lbc-bndy.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
 """
-Combine LBC files from GFS into one
+Add variables from chem LBC file into GFS LBC files in place.
+Like `ncks -A a.nc b.nc`, where `a.nc` is the chem LBC file
+and `b.nc` is one of the GFS LBC files.
 """
 import argparse
+import itertools
+from glob import glob
+from pathlib import Path
 
 import netCDF4 as nc4
 
-parser = argparse.ArgumentParser(description="Combine LBCs.")
+parser = argparse.ArgumentParser(description="Add chem to LBCs.")
 
 parser.add_argument(
-    "--hours",
+    "--chem",
+    help="Chem LBC file.",
+    required=True,
+)
+
+parser.add_argument(
+    "--met",
     nargs="+",
     help=(
-        "Forecast hour strings (array, don't surround it with outer quotes). "
-        "Not including forecast hour 0. "
-        "Example: `003 006`."
+        "Meteo (GFS) LBC files, to which the chem variables will be added."
+        "File name may have the pattern `gfs_bndy.tile?.???.nc`."
     ),
     required=True,
 )
@@ -22,58 +32,63 @@ parser.add_argument(
 args = parser.parse_args()
 print(args)
 
-fh_strings = args.hours
-fh_strings.insert(0, "0" * len(fh_strings[0]))
-print(fh_strings)
+rm = True  # Create new files with chem vars removed instead
 
 # TODO: probably easier to build the array of input fps and pass it to this script
 files = []
-ofp = "t.nc"
+files = sorted(glob("./tmp_AQM_LBCS/gfs_bndy.tile7.???.nc"))
+chem_fp = "./tmp_AQM_LBCS/gfs_bndy_chem_08.tile7.000.nc"
 
-# time
-# assuming each file has only one time
-times = []
-srcs = []
-for f in files:
-    print(f)
-    ds = nc4.Dataset(f)
-    times_num_f = ds["time"][:]
-    assert times_num_f.size == 1
-    times.append(times_num_f[0])
+# Open files
+chem = nc4.Dataset(chem_fp, "r")
+mets = {
+    p: nc4.Dataset(p, "a")
+    for p in files
+}
+rms = {}
 
-    srcs.append(ds)
+met0 = mets[files[0]]
+allowed_dims = set(met0.dimensions)
 
-src0 = srcs[0]
-dst = nc4.Dataset(ofp, "w", format="NETCDF4")
+for name, variable in chem.variables.items():
+    print(name)
+    assert set(variable.dimensions) < allowed_dims
+    for met_fp, met in mets.items():
+        if name not in met.variables:
+            # add
+            met.createVariable(name, variable.dtype, variable.dimensions)
+            met[name].setncatts({key: getattr(variable, key) for key in variable.ncattrs()})
+            met[name][:] = chem[name][:]
+            print(f"-> {met_fp}")
 
-# dims
-for name, dimension in src0.dimensions.items():
-    dst.createDimension(name, len(dimension) if not dimension.isunlimited() else None)
-assert dst.dimensions["time"].isunlimited()
+if rm:
+    met_names = [vn for vn in met0.variables if vn not in chem.variables]
+    dims_needed = set(itertools.chain.from_iterable(met0[vn].dimensions for vn in met_names))
+    for met_fp, met in mets.items():
+        p = Path(met_fp)
+        p_new = p.with_stem(f"{p.stem}_clean")
+        ds = nc4.Dataset(p_new, "w", format="NETCDF4")
+        print(p_new.as_posix())
 
-# coords
-time_units = src0["time"].units
-coord_names = ["time", "latitude", "longitude"]
-for name in coord_names:
-    dst.createVariable(name, src0[name].dtype, src0[name].dimensions)
-    dst[name].setncatts({key: getattr(src0[name], key) for key in src0[name].ncattrs()})
-    if name == "time":
-        dst[name][:] = times
-    else:
-        dst[name][:] = src0[name][:]
-assert dst.variables["time"].size == len(srcs)
+        # dims
+        for name, dimension in met.dimensions.items():
+            ds.createDimension(name, len(dimension) if not dimension.isunlimited() else None)
 
-# variables
-for name, variable in src0.variables.items():
-    if name in coord_names:
-        continue
-    dst.createVariable(name, variable.dtype, variable.dimensions)
-    dst[name].setncatts({key: getattr(variable, key) for key in variable.ncattrs()})
+        # variables
+        for name in met_names:
+            v = met[name]
+            v_new = ds.createVariable(name, v.dtype, v.dimensions)
+            v_new.setncatts({key: getattr(variable, key) for key in v.ncattrs()})
+            v_new[:] = v[:]
+            print(f"- {name}")
+            
+        ds.close()
 
-# data
-for i, src in enumerate(srcs):
-    for name in src.variables:
-        if name in coord_names:
-            continue
-        dst[name][i] = src[name][:]
+
+# Close
+chem.close()
+for met in mets.values():
+    met.close()
+
+raise SystemExit(0)
 
