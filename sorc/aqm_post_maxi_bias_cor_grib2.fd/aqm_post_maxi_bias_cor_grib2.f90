7c1c79360620f742fdbab52ce79d541334d18a42
kai wang <kai.wang@dlogin01.dogwood.wcoss2.ncep.noaa.gov>
2026-06-02 16:18:02 +0000

    continue to fix the encoding issue for bias-corrected products

--- sorc/aqm_post_maxi_bias_cor_grib2.fd/aqm_post_maxi_bias_cor_grib2.f90
+++ sorc/aqm_post_maxi_bias_cor_grib2.fd/aqm_post_maxi_bias_cor_grib2.f90
@@ -445,7 +445,7 @@ program aqm_post_maxi_bias_cor_grib2_793
       ipdstmpllen=29        ! pdt template length
 !    ipdstmpl(1)=13          ! catogory
 !    ipdstmpl(2)=193         ! parameter
-      ipdstmpl(3)=2         ! Type of generating process (Table 4.3) (0:ana, 1:ic, 2:fcst)
+      ipdstmpl(3)=3         ! Type of generating process (Table 4.3) (0:ana, 1:ic, 2:fcst, 3:bias-cor)
       ipdstmpl(4)=0         ! Background generating process identifier
       ipdstmpl(5)=211       ! Analysis or forecast generating process identified (ON388TableA)
       ipdstmpl(6)=0         ! Hours of observational data cutoff after reference time
